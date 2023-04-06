3fccfba45ed31237599bf4bdfc8d56714025e54c
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-04-06 16:28:16 -0300

    Condicional encadeada

--- js/05condicionais.js
+++ js/05condicionais.js
@@ -30,6 +30,8 @@ const mediaMinima = 7;
 const limiteDeFaltas = 10;
 
 /* Se a média obtida pelo aluno(a) for maior ou igual á média mínima da escola, então ele estará reprovado. Caso contrário, ele estará reprovado. */
+
+/* Condicional Composta */
 if (media >= mediaMinima && faltas < limiteDeFaltas) {
     console.log("aprovado");
 } else {  //Não precisa refazer o cálculo porque não tem outras condições.
@@ -44,6 +46,36 @@ if (media >= mediaMinima && faltas < limiteDeFaltas) {
 */
 
 
+
+/* Condicional encadeada: mais de duas condições provavelmente. */
+
+/*Algoritmo para análise de desempenho:
+- Se a média do aluno for menor que 3 -> PÉSSIMO
+- Se a média for menor que 5 -> RUIM
+- Se a média for menor que 7 -> REGULAR
+- Se a média for menor que 9 -> BOM
+- Nenhuma das anteriores -> ÓTIMO
+*/
+
+
+// Váriavel que irá armazenar texto/desempenho
+let desempenho; //começa indefinida
+
+/* O else if significa "senão se" ou seja, se a nota não for dessa maneira, será dessa. */
+if(media < 3){
+    desempenho = "PÉSSIMO";
+} else if (media < 5) {
+    desempenho = "RUIM";
+} else if (media < 7) {
+    desempenho = "REGULAR";
+} else if (media < 9) {
+    desempenho = "BOM";
+} else {
+    desempenho = "ÓTIMO";
+}
+
+/* Sem o console log os dados ficam armazenados na memória */
+console.log(desempenho);
 
 
 
