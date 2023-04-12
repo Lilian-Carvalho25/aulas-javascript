7027971750ad669272e11ceb732ba8069e3f1b07
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-04-11 23:26:08 -0300

    Correção

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -1,11 +1,10 @@
-const nome = prompt("Por favor,informe seu nome.");
+const nome = prompt("Por favor, informe seu nome.");
 const idade = prompt("Por favor, informe sua idade.");
-alert(`${recado}`)
-let recado;
+
 if (idade >= 18 && idade < 60) {
-      recado = (`${nome}, você é uma pessoa adulta.`)
+      alert(`${nome}, você é uma pessoa adulta.`)
 } else if (idade >= 60) {
-    recado = (`${nome}, você é uma pessoa idosa.`)
+      alert(`${nome}, você é uma pessoa idosa.`)
 } else {
-    recado = (`${nome}, você é uma pessoa menor de idade.`)
+      alert(`${nome}, você é uma pessoa menor de idade.`)
 }
