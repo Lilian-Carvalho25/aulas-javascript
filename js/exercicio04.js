5b2d395ff1e30955c21e464ef03d4dd9e95325c8
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-04-20 22:07:36 -0300

    Quase finalizando o exercício 4

--- js/exercicio04.js
+++ js/exercicio04.js
@@ -5,30 +5,31 @@ const botao3 = document.querySelector("#tema-roxo");
 const pBlue = document.querySelector("#descricao");
 const pGreen = document.querySelector("#descricao");
 const pOrange = document.querySelector("#descricao");
+let p = document.createElement("p");
 
 botao1.addEventListener("click", function(){
     tema.classList.toggle("tema-azul");
 
-    let p = document.createElement("p");
     p.innerHTML = `Azul significa Harmonia e Maturidade.`;
     pBlue.appendChild(p);
     pBlue.style.color = "lightblue"
+    return
 });
 
 botao2.addEventListener("click", function(){
     tema.classList.toggle("tema-verde");
 
-    let p = document.createElement("p");
     p.innerHTML = `Verde significa Saúde de Esperança.`;
     pGreen.appendChild(p);
     pGreen.style.color = "lightgreen"
+    return
 });
 
 botao3.addEventListener("click", function(){
     tema.classList.toggle("tema-roxo");
 
-    let p = document.createElement("p");
     p.innerHTML = `O roxo está ligado às profundezas da mente.`;
     pOrange.appendChild(p);
     pOrange.style.color = "white";
+    return
 });
