5848cbe5dfd032c18ee89640384cffe92aadf5da
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-04-21 13:53:42 -0300

    Update exercício

--- js/exercicio04.js
+++ js/exercicio04.js
@@ -7,22 +7,53 @@ const pGreen = document.querySelector("#descricao");
 const pOrange = document.querySelector("#descricao");
 let p = document.createElement("p");
 
+// botao1.addEventListener("click", function(){
+//     tema.classList.toggle("tema-azul");
+
+//     p.innerHTML = `Azul significa Harmonia e Maturidade.`;
+//     pBlue.appendChild(p);
+//     pBlue.style.color = "lightblue"
+// });
+
 botao1.addEventListener("click", function(){
+
+
     tema.classList.toggle("tema-azul");
+    if (tema.classList.toggle("tema-verde")) {
+        tema.classList.remove("tema-verde");
+    }
+
+    if (tema.classList.toggle("tema-roxo")) {
+        tema.classList.remove("tema-roxo");
+    }
 
     p.innerHTML = `Azul significa Harmonia e Maturidade.`;
     pBlue.appendChild(p);
     pBlue.style.color = "lightblue"
-    return
 });
 
+// botao2.addEventListener("click", function(){
+//     tema.classList.toggle("tema-verde");
+
+//     p.innerHTML = `Verde significa Saúde de Esperança.`;
+//     pGreen.appendChild(p);
+//     pGreen.style.color = "lightgreen"
+// });
+
 botao2.addEventListener("click", function(){
+    if (tema.classList.toggle("tema-roxo")) {
+        tema.classList.remove("tema-roxo");
+    }
+
+    if (tema.classList.toggle("tema-azul")) {
+        tema.classList.remove("tema-azul");
+    }
     tema.classList.toggle("tema-verde");
 
     p.innerHTML = `Verde significa Saúde de Esperança.`;
     pGreen.appendChild(p);
     pGreen.style.color = "lightgreen"
-    return
+
 });
 
 botao3.addEventListener("click", function(){
@@ -31,5 +62,4 @@ botao3.addEventListener("click", function(){
     p.innerHTML = `O roxo está ligado às profundezas da mente.`;
     pOrange.appendChild(p);
     pOrange.style.color = "white";
-    return
 });
